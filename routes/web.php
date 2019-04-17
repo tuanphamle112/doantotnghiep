fdf49895364f6fe8586f5f16b3ddcb09cd3abd07
TuanPhamLe <tuanphamle@ubuntu.ubuntu-domain>
2019-04-17 13:20:37 +0700

    login log out middeware-fix convention3

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+// admin route
+Route::group(['middleware' => 'admin', 'prefix' => 'admin'], function () {
+    Route::resource('users', 'Admin\UserController');
+});
